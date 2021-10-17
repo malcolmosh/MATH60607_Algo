9695044308a43ecf5bbe4c4869c506bed17d51ff
emanuel140697 <emanuelsenaylussier@hotmail.com>
2021-10-17 16:52:10 -0400

    Update main_test_emanuel.py

--- main_test_emanuel.py
+++ main_test_emanuel.py
@@ -1,7 +1,7 @@
 from All_class.class_dataset import dataset
 
 data = dataset()
+print(data.list_files())
 data_selection = data.data_selection_list("salle_test50.txt",True)
 print(data_selection)
-
 
