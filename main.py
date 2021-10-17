164648ed61a5f1e7069a1fdca31ca3d2ebbeec0d
emanuel140697 <emanuelsenaylussier@hotmail.com>
2021-10-17 11:42:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,9 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Oct 15 15:44:38 2021
-
-@author: equipe_algo
-"""
 import pathlib
 import os
+from All_class.class_dataset import dataset
 
-#Aller chercher dynamiquement le dossier Data et créer une liste des fichiers txt
-path_origin = str(pathlib.Path(__file__).parent.resolve())
-path_data = path_origin + "\Data"
-data_files = os.listdir(path_data)
-data_files_txt = []
-for files in data_files:
-    if files[-4:] == ".txt":
-        data_files_txt.append(files)
-print(data_files)
-print(data_files_txt)
-
-#Créer une liste de liste pour chaque source data
-data_dict = {}
-for files in data_files_txt:
-    data_dict[files][0:-4] = 1
-print(data_dict)
-# for each in data_files_txt:
-#     exec(f'data_{each} = []')
-#     # list1 = []
-#     # list2 = []
-#     # list3 = []
-# # with open('test.txt', 'r') as f:
-# #     content = f.readlines()
-# #     for x in content:
-# #         row = x.split()
-# #         list1.append(int(row[0]))
-# #         list2.append(int(row[1]))
-# #         list3.append(int(row[2]))
+data = dataset()
+data_selection = data.data_selection_list("salle_test50.txt",True)
+print(data_selection)
 
 
