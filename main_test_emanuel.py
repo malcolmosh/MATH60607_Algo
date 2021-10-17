19e49012d9d91df3d0e63c48e509b7cc9b3a1e6c
emanuel140697 <emanuelsenaylussier@hotmail.com>
2021-10-17 19:08:12 -0400

    Update main_test_emanuel.py

--- main_test_emanuel.py
+++ main_test_emanuel.py
@@ -1,7 +1,15 @@
 from All_class.class_dataset import dataset
+import random
+import pygame
 
+def optimization(data_list):
+    for chair in range(0,len(data_list)):
+        data_list[chair].append(random.randint(0,1))
+        
 data = dataset()
 print(data.list_files())
 data_selection = data.data_selection_list("salle_test50.txt",True)
 print(data_selection)
 
+optimization(data_selection)
+print(data_selection)
