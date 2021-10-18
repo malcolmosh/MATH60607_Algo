386a1bbc4ef60fa32691296c14d0589040df2021
emanuel140697 <emanuelsenaylussier@hotmail.com>
2021-10-18 13:51:03 -0400

    Update main_test_emanuel.py

--- main_test_emanuel.py
+++ main_test_emanuel.py
@@ -1,15 +1,20 @@
-from All_class.class_dataset import dataset
-import random
+from All_class.class_dataset import Dataset
+from All_class.class_optimization_random import Optimization_random
 import pygame
 
-def optimization(data_list):
-    for chair in range(0,len(data_list)):
-        data_list[chair].append(random.randint(0,1))
-        
-data = dataset()
+#Assigner la class Dataset à la variable "data"     
+data = Dataset()
+#Print le résultat de la fonction list_files() de la classe Dataset
 print(data.list_files())
-data_selection = data.data_selection_list("salle_test50.txt",True)
-print(data_selection)
+#Assigner à le résultat de la fonction data_selection_list() de la classe Dataset à la variable "data_selection" 
+data_selection = data.data_selection_list("salle_test50.txt")
+for chair in data_selection:
+    print(chair)
 
-optimization(data_selection)
-print(data_selection)
+#Assigner la classe Optimization_random à la variable "optimization"
+optimization = Optimization_random(data_selection)
+#Assigner à le résultat de la fonction optimize() de la classe Optimization_random à la variable "optimization_export"
+#Choix de la valeur 25 pour le paramètre optionel "rate_use"
+optimization_export = optimization.optimize(25)
+for chair in optimization_export:
+    print(chair)
