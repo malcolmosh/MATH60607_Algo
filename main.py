95de8795a4708326cdd03c5da2e6dba0e1ca5a2f
emanuel140697 <emanuelsenaylussier@hotmail.com>
2021-11-02 08:47:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
-from All_class.class_dataset import Dataset
+from All_class.class_dataset import Salles
 from All_class.class_optimization_random import Optimization_random
-from All_class.class_application import Application
+from All_class.class_application_tkinter import Application
 import random, pathlib, os, pygame, math
 from sys import exit
 
-data = Dataset()
+data = Salles()
 # print(data.list_files())
-data_selection = data.chairs_list("salle_test108.txt")
-# for chair in data_selection:
-#     print(chair)
-room_info = data.room_info("salle_test108_info.txt")
-#print(room_info)
-optimization = Optimization_random(data_selection)
-optimization_export = optimization.optimize(25)
+# data_selection = data.chairs_list("Salle Saine Marketing.txt")
+# print(data_selection)
+# room_info = data.room_info("Salle Saine Marketing Info.txt")
+# # #print(room_info)
+# optimization = Optimization_random(data_selection)
+# optimization_export = optimization.optimize(25)
 # for chair in optimization_export:
 #     print(chair)
 
 
-app = Application(optimization_export, room_info)
-app.game_loop()
+app = Application()
