e9c9a3bafda2bf6102ea957e994bac32c955b827
golmah <92604494+golmah@users.noreply.github.com>
2021-11-07 18:22:19 -0500

    add

--- All_class/class_optimization_des_sections.py
+++ All_class/class_optimization_des_sections.py
@@ -3,7 +3,7 @@ import pandas as pd
 import time
 
 class Optimization_des_sections():
-    def __init__(self, data,distance,iterations=500, maximum_time=5):
+    def __init__(self, data,distance,iterations=1, maximum_time=5):
         self.data = data
         self.distance = distance
         self.iterations = iterations
